dba01be9ae420ff07310a4f207e206d2bf66e8f0
박지훈 <jihoon050629@naver.com>
2026-08-07 23:24:26 +0900

    [Passed] 중앙값 구하기 (50ms, 16MB)

---
+++ Codetree/trail0/중앙값 구하기/find-the-median.py
@@ -0,0 +1,17 @@
+a,b,c = map(int,input().split())
+if a>b:
+    if b>c:
+        print(b)
+    else:
+        if c>a:
+            print(a)
+        else:
+            print(c)
+else:
+    if a>c:
+        print(a)
+    else:
+        if b>c:
+            print(c)
+        else:
+            print(b)
