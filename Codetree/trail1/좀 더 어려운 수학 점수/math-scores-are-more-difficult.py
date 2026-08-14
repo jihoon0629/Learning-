5e326a19393924fde71b5b0c2b583628275d9bda
박지훈 <jihoon050629@naver.com>
2026-08-14 17:27:17 +0900

    [Passed] 좀 더 어려운 수학 점수 (46ms, 16MB)

---
+++ Codetree/trail1/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,11 @@
+a1,a2=map(int,input().split())
+b1,b2=map(int,input().split())
+if a1>b1:
+    print('A')
+elif a1==b1:
+    if a2>b2:
+        print('A')
+    else:
+        print('B')
+else:
+    print('B')
