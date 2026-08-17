e29c74ddafb7fe10f88e6814101216c2da19c536
박지훈 <jihoon050629@naver.com>
2026-08-17 13:13:01 +0900

    [Passed] 별표 출력하기 6 (48ms, 16MB)

---
+++ Codetree/trail1/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,13 @@
+n=int(input())
+for i in range(1,n+1):
+    for j in range(i-1):
+        print(' ',end=' ')
+    for j in range(2*n-2*i+1):
+        print('*',end=' ')
+    print()
+for i in range(n-1,0,-1):
+    for j in range(i-1):
+        print(' ',end=' ')
+    for j in range(2*n-2*i+1):
+        print('*',end=' ')
+    print()
