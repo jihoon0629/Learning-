09034d97442cea580376a3f1710a27195e2bc072
박지훈 <jihoon050629@naver.com>
2026-08-17 23:32:09 +0900

    [Passed] 알파벳 출력하기 2 (52ms, 16MB)

---
+++ Codetree/trail1/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,12 @@
+n=int(input())
+cnt=ord('A')
+for i in range(n):
+    for j in range(i):
+        print(' ',end=' ')
+    for j in range(n-i):
+        print(chr(cnt),end=' ')
+        if cnt==ord('Z'):
+            cnt=ord('A')
+        else:
+            cnt+=1
+    print()
