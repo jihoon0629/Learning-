b1f7a1c25bd2398f67526a7fd9eb808d1abc4d19
박지훈 <jihoon050629@naver.com>
2026-08-17 13:30:46 +0900

    [Passed] 별표 출력하기 13 (53ms, 16MB)

---
+++ Codetree/trail1/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range(1,2*n+1):
+    if i%2==1:
+        for j in range(n-(i-1)//2):
+            print('* ',end='')
+    else:
+        for j in range(i//2):
+            print('* ',end='')
+    print()
