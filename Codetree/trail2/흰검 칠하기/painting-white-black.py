ecd83b80ed7b86887665843f6d210dfbdc84dc73
박지훈 <jihoon050629@naver.com>
2026-09-26 02:21:39 +0900

    [Passed] 흰검 칠하기 (49ms, 16MB)

--- Codetree/trail2/흰검 칠하기/painting-white-black.py
+++ Codetree/trail2/흰검 칠하기/painting-white-black.py
@@ -1,37 +1,35 @@
 n = int(input())
 arr = {}
-black = {}
-white = {}
-cnt = 0
+b = {}
+w = {}
+idx = 0
 for i in range(n):
-    a,b = input().split()
+    a, d = input().split()
     a = int(a)
-    if b == 'R':
-        for j in range(cnt,cnt+a):
-            black[j] = black.get(j,0) + 1
-            white[j] = white.get(j,0)
-            if black[j] >= 2 and white[j] >= 2:
-                arr[j] = 'gray'
-            else:
-                arr[j] = 'black'
-        cnt += (a-1)
-    else:
-        for j in range(cnt-a+1,cnt+1):
-            black[j] = black.get(j,0)
-            white[j] = white.get(j,0) + 1
-            if white[j] >= 2 and black[j] >= 2:
+    if d == 'L':
+        for j in range(idx-a+1,idx+1):
+            w[j] = w.get(j, 0) + 1
+            b[j] = b.get(j, 0)
+            if w[j] >= 2 and b[j] >= 2:
                 arr[j] = 'gray'
             else:
                 arr[j] = 'white'
-        cnt -= (a-1)
-gray = 0
-black = 0
-white = 0
-for i in arr.values():
-    if i == 'gray':
-        gray += 1
-    elif i == 'black':
-        black += 1
+        idx = idx - a + 1
     else:
-        white += 1
-print(white, black, gray)
+        for j in range(idx,idx+a):
+            w[j] = w.get(j, 0)
+            b[j] = b.get(j, 0) + 1
+            if w[j] >= 2 and b[j] >= 2:
+                arr[j] = 'gray'
+            else:
+                arr[j] = 'black'
+        idx = idx + a - 1
+g, b, w = 0, 0, 0
+for i in arr:
+    if arr[i] == 'gray':
+        g += 1
+    elif arr[i] == 'black':
+        b += 1
+    else:
+        w += 1
+print(w, b, g)
