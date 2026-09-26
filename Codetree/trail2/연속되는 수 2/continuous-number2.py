dcfe902ca2cd15b37a18b6325ea723a613fb7661
박지훈 <jihoon050629@naver.com>
2026-09-26 14:38:36 +0900

    [Passed] 연속되는 수 2 (70ms, 16MB)

--- Codetree/trail2/연속되는 수 2/continuous-number2.py
+++ Codetree/trail2/연속되는 수 2/continuous-number2.py
@@ -9,8 +9,8 @@ for i in range(n):
         if cnt > max_cnt:
             max_cnt = cnt
         cnt=1
-    elif arr[i] == arr[i-1]:
+    else:
         cnt+=1
-if cnt > max_cnt:
-    max_cnt = cnt
+    if cnt > max_cnt:
+        max_cnt = cnt
 print(max_cnt)
