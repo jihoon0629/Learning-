253b981e5843f8ef5105653c58486782bbd3b7d9
박지훈 <jihoon050629@naver.com>
2026-09-26 14:22:23 +0900

    [Passed] 계속 중첩되는 사각형 (61ms, 17MB)

---
+++ Codetree/trail2/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -0,0 +1,19 @@
+def f(a,b,c,d,color):
+    for i in range(a+100,c+100):
+        for j in range(b+100,d+100):
+            arr[i][j] = color
+    return arr
+arr = [[None for _ in range(201)] for _ in range(201)]
+n = int(input())
+for i in range(n):
+    a,b,c,d = map(int,input().split())
+    if i%2==0:
+        f(a,b,c,d,'red')
+    else:
+        f(a,b,c,d,'blue')
+cnt=0
+for i in range(201):
+    for j in range(201):
+        if arr[i][j] == 'blue':
+            cnt+=1
+print(cnt)
