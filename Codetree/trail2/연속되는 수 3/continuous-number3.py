49da7a3fb4eda69e58001cee79550c8185d7da9d
박지훈 <jihoon050629@naver.com>
2026-09-26 14:41:14 +0900

    [Passed] 연속되는 수 3 (70ms, 17MB)

---
+++ Codetree/trail2/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+max_cnt = 0
+for i in range(n):
+    if i==0 or arr[i] * arr[i-1] < 0:
+        cnt = 1
+    else:
+        cnt+=1
+    if cnt > max_cnt:
+        max_cnt = cnt
+print(max_cnt)
